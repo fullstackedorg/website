cec5c12350e50fe1801b2332a938866a40e1222a
cplepage <lepage.charlesphilippe@gmail.com>
2024-09-27 07:56:19 -0400

    ref as param

--- download/script.js
+++ download/script.js
@@ -14,6 +14,12 @@ async function getMainVersion() {
     return version;
 }
 
+async function getLatestCommit() {
+    const response = await fetch("https://api.github.com/repos/fullstackedorg/editor/git/refs/heads/main");
+    const { object: { sha } } = await response.json();
+    return sha;
+}
+
 const downloadBaseURL = "https://files.fullstacked.org/releases";
 getLatestRelease().then((version) => {
     document.querySelector("#stable-version").innerText = version;
@@ -67,54 +73,61 @@ getLatestRelease().then((version) => {
         `/fullstacked-${version}-linux-x64.rpm`;
 });
 
-getMainVersion().then((version) => {
-    document.querySelector("#beta-version").innerText = version;
+Promise.all([
+    getMainVersion(),
+    getLatestCommit()
+]).then(([
+    version,
+    commit
+]) => {
+    const ref = commit.slice(0, 8);
+    document.querySelector("#beta-version").innerHTML = version + `<br /><small>${ref} (main)</small>`;
 
     document.querySelector("#beta-darwin-arm64").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-darwin-arm64.zip`;
+        `/fullstacked-${version}-darwin-arm64.zip?v=${ref}`;
 
     document.querySelector("#beta-darwin-x64").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-darwin-x64.zip`;
+        `/fullstacked-${version}-darwin-x64.zip?v=${ref}`;
 
     document.querySelector("#beta-win32-arm64").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-win32-arm64.zip`;
+        `/fullstacked-${version}-win32-arm64.zip?v=${ref}`;
 
     document.querySelector("#beta-win32-x64").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-win32-x64.zip`;
+        `/fullstacked-${version}-win32-x64.zip?v=${ref}`;
 
     document.querySelector("#beta-linux-arm64-deb").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-linux-arm64.deb`;
+        `/fullstacked-${version}-linux-arm64.deb?v=${ref}`;
 
     document.querySelector("#beta-linux-x64-deb").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-linux-x64.deb`;
+        `/fullstacked-${version}-linux-x64.deb?v=${ref}`;
 
     document.querySelector("#beta-linux-arm64-rpm").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-linux-arm64.rpm`;
+        `/fullstacked-${version}-linux-arm64.rpm?v=${ref}`;
 
     document.querySelector("#beta-linux-x64-rpm").href =
         downloadBaseURL +
         "/" +
         version +
-        `/fullstacked-${version}-linux-x64.rpm`;
-});
+        `/fullstacked-${version}-linux-x64.rpm?v=${ref}`;
+})
