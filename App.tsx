70eb2ac2c569d660257f8dccacfda93d824882b5
cplepage <lepage.charlesphilippe@gmail.com>
2026-03-18 14:17:35 -0400

    fixed cropped overflow blob

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
     return (
         <div className="min-h-screen flex flex-col bg-[#020617] selection:bg-sky-500/30">
             <Navbar />
-            <main className="flex-grow">
+            <main className="flex-grow overflow-hidden">
                 <Hero />
                 <HowItWorks />
             </main>
